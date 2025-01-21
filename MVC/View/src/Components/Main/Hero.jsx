d963c8e27ef0fd01eedd01fbf4dd74b531ad17a0
Kenko <kerimlikenan00@gmail.com>
2025-01-21 14:18:55 +0400

    v1.2

---
+++ MVC/View/src/Components/Main/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Hero.css'
+function Hero() {
+  return (
+    <div id='hero'>
+        <div className="hero-cont">
+            <h2>BANKING SOLUTIONS</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quis natus neque a? At soluta, id deserunt eveniet cum temporibus?</p>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
--- MVC/View/src/Pages/Home/index.jsx
+++ MVC/View/src/Pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Hero from '../../Components/Main/Hero'
 
 import { Helmet } from 'react-helmet'; 
 
@@ -9,6 +10,7 @@ function index() {
         <title>Home</title> 
         <meta name="description" content="Welcome to our homepage." /> 
       </Helmet>
+    <Hero/>
 
     </div>
   )
