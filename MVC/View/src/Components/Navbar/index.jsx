a463c84cc040771f1aa20a1bb0c4326038376dad
Kenko <kerimlikenan00@gmail.com>
2025-01-21 14:04:58 +0400

    v.1.1

--- MVC/View/src/Components/Navbar/index.jsx
+++ MVC/View/src/Components/Navbar/index.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
-import './Navbar.css'
-function index() {
+import React, { useState } from 'react';
+import './Navbar.css';
+import { FaFacebookF } from "react-icons/fa6";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Index() {
+
+
+
   return (
     <div>
-      
+      <nav className="navbar">
+        <div className="navbar-container">
+          <a href="/" className="navbar-logo">Banker</a>
+          <ul >
+            <li><a href="#about">About Us</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#blog">Blog</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <FaFacebookF/>
+            <FaTwitter/>
+            <FaLinkedinIn/>
+          </ul>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default index
+export default Index;
